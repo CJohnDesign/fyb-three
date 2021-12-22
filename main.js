98df57d97e828f9666b2cbf45c694d339829ece8
Chris Johnston <chrisjohnston@Chriss-MacBook-Pro.local>
2021-12-21 21:06:14 -0500

    MVP - light up donut

---
+++ main.js
@@ -0,0 +1,134 @@
+import './style.css';
+import * as THREE from 'three';
+
+// Setup
+
+if ( WEBGL.isWebGLAvailable() ) {
+
+var
+  camera,
+  light;
+
+var mouse = {
+    x: 0,
+    y: 0
+  };
+
+const scene = new THREE.Scene();
+
+var screenWidth = window.innerWidth,
+    screenHeight = window.innerHeight,
+    viewAngle = 75,
+    nearDistance = 0.1,
+    farDistance = 1000;
+
+  camera = new THREE.PerspectiveCamera(viewAngle, screenWidth / screenHeight, nearDistance, farDistance);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
+
+// Torus
+
+const geometry = new THREE.TorusGeometry(20, 3, 200, 1000);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 0, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+
+
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+// function addStar() {
+//   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+//   const star = new THREE.Mesh(geometry, material);
+
+//   const [x, y, z] = Array(3)
+//     .fill()
+//     .map(() => THREE.MathUtils.randFloatSpread(100));
+
+//   star.position.set(x, y, z);
+//   scene.add(star);
+// }
+
+// Array(200).fill().forEach(addStar);
+
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load('./space.jpg');
+scene.background = spaceTexture;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = -50;
+  camera.position.x = -1 //(t * -0.025) -30;
+  camera.position.y = 10;
+  camera.lookAt(1,0,0)
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
+document.addEventListener('mousemove', onMouseMove, false);
+
+// Follows the mouse event
+function onMouseMove(event) {
+
+  // Update the mouse variable
+  event.preventDefault();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Make the sphere follow the mouse
+  var vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
+  vector.unproject(camera);
+  var dir = vector.sub(camera.position).normalize();
+  var distance = -camera.position.z / dir.z;
+  var pos = camera.position.clone().add(dir.multiplyScalar(distance));
+  //mouseMesh.position.copy(pos);
+
+  pointLight.position.copy(new THREE.Vector3(pos.x, 0, pos.z + -20));
+};
+
+
+} else {
+
+	const warning = WEBGL.getWebGLErrorMessage();
+	document.getElementById( 'bg' ).appendChild( warning );
+
+}
